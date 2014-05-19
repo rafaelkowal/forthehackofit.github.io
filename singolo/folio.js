eff7e1ddd6ada59517fa184ff1fcb298f8896fc3
Ady Ngom <adyngom.com@gmail.com>
2014-05-18 22:39:55 -0400

    * Adding singolo one page portfolio

---
+++ singolo/folio.js
@@ -0,0 +1,50 @@
+var Folio = [
+  {
+    img: "images/thumbnails/img1.jpg",
+    caption: "image 1"
+  },
+  {
+    img: "images/thumbnails/img2.jpg",
+    caption: "image 2"
+  },
+  {
+    img: "images/thumbnails/img3.jpg",
+    caption: "image 3"
+  },
+  {
+    img: "images/thumbnails/img4.jpg",
+    caption: "image 4"
+  },
+  {
+    img: "images/thumbnails/img5.jpg",
+    caption: "image 5"
+  },
+  {
+    img: "images/thumbnails/img6.jpg",
+    caption: "image 6"
+  },
+  {
+    img: "images/thumbnails/img7.jpg",
+    caption: "image 7"
+  },
+  {
+    img: "images/thumbnails/img8.jpg",
+    caption: "image 8"
+  },
+  {
+    img: "images/thumbnails/img9.jpg",
+    caption: "image 9"
+  },
+  {
+    img: "images/thumbnails/img10.jpg",
+    caption: "image 10"
+  },
+  {
+    img: "images/thumbnails/img11.jpg",
+    caption: "image 11"
+  },
+  {
+    img: "images/thumbnails/img12.jpg",
+    caption: "image 12"
+  }
+];
