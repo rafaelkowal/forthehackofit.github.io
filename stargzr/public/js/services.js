d8fa327d745a6afc4e453bd4acedc99342eef797
Ady Ngom <adyngom.com@gmail.com>
2014-07-06 02:14:19 -0400

    * CSS changes * added movie schema * querying movie items

--- stargzr/public/js/services.js
+++
@@ -1,7 +0,0 @@
-angular.module('starter.services', ['ngResource'])
-.factory('Actors', function($resource) {
-  return $resource('api/actors/');
-})
-.factory('Actor', function($resource) {
-  return $resource('/api/actors/:_id');
-});
--- stargzr/server.js
+++ stargzr/server.js
@@ -16,32 +16,73 @@ mongoose.connection.on('error', console.error.bind(console, 'connection error:')
 mongoose.set('_debug', true);
 
 var request = require('request');
+var async = require('async');
 // d9e6e07490160fe90b54a6609dbde93b
 // https://api.themoviedb.org/3/movie/550?api_key=
 
 
-var actorSchema = new mongoose.Schema({
+var starSchema = new mongoose.Schema({
     _id: Number,
     name: String,
-    also_known_as: String,
-    summary: String,
-    birthday: Date,
-    place_of_birth: String,
-    profile_path: String,
     portrait: String,
-    biography: String,
     subscribers: [{
         type: mongoose.Schema.Types.ObjectId, ref: 'User'
     }],
-    movies: [{
-        type: mongoose.Schema.Types.ObjectId, ref: 'Movie'
-    }]
+    movies: [{}],
+    aka: String,
+    summary: String,
+    birthday: Date,
+    birthplace: String,
+    profile: String,
+    biography: String,
+    slug: String
+});
+
+starSchema.pre('save', function(next) {
+  this.slug = slugify(this.name);
+  this.movies.map(function(movie) {
+    movie.slug = slugify(movie.title);
+  });
+  next();
 });
 
 var userSchema = new mongoose.Schema({
     email: { type: String, unique: true },
     password: String
 });
+
+var movieSchema = new mongoose.Schema({
+  _id: Number,
+  backdrop_path: String,
+  genres: [{}],
+  imdb_id: String,
+  original_title: String,
+  title: { type: String, required: true },
+  overview: String,
+  poster_path: String,
+  release_date: Date,
+  status: String,
+  tagline: String,
+  cast: [{}],
+  vote_average: Number,
+  vote_count: Number,
+  slug: { type: String, unique: true }
+});
+
+movieSchema.pre('save', function(next) {
+  this.slug = slugify(this.title);
+  next();
+});
+
+function slugify(text)
+{
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
 
 userSchema.pre('save', function(next) {
   var user = this;
@@ -63,46 +104,81 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
   });
 };
 
-var Actor = mongoose.model('Actor', actorSchema);
+var Star = mongoose.model('Star', starSchema);
 var User = mongoose.model('User', userSchema);
+var Movie = mongoose.model('Movie', movieSchema);
 
 /*** Schedule cron tasks ***/
 var agenda  = require('agenda')({ db: { address: "127.0.0.1:27017/stargzr" } });
 var _ = require('lodash');
+var apiUrl = "https://api.themoviedb.org/3/",
+    apiKey = "d9e6e07490160fe90b54a6609dbde93b";
 
-agenda.define('get latest', function(job, done) {
-    request('https://api.themoviedb.org/3/'+job.attrs.data+'?api_key=d9e6e07490160fe90b54a6609dbde93b', function (error, response, body) {
-        if (error) return next(error);
-      //console.log('Status:', response.statusCode);
-      //console.log('Headers:', JSON.stringify(response.headers));
-      //console.log('Response:', JSON.parse(body).results);
-      var data = JSON.parse(body);
-      _.each(data.results, function(person) {
-        var actor = new Actor({
-            _id: person.id,
-            name: person.name,
-            also_known_as: person.also_known_as,
-            biography: person.biography,
-            portrait: person.profile_path,
-            birthday: person.birthday,
-            place_of_birth: person.place_of_birth
+agenda.define('get latest', { priority: 'high', concurrency: 10 }, function(job, done) {
+    async.waterfall([
+      /* get the basic start info */
+      function (callback) {
+        request(apiUrl+job.attrs.data+'?api_key='+apiKey, function (err, response, body) {
+            if (err) return next(err);
+            var data = JSON.parse(body);
+            //console.log(data);
+            _.each(data.results, function(person) {
+              var star = new Star({
+                  _id: person.id,
+                  name: person.name,
+                  portrait: person.profile_path
+              });
+              callback(err, star);
+              //console.log(star);
+            }); 
+          });
+      },
+      /* then get the advance info */
+      function (star, callback) {
+        //console.log(star);
+        request(apiUrl+"person/"+star._id+"?api_key="+apiKey, function (err, response, body) {
+          if (err) return next(err);
+          var meta = JSON.parse(body);
+              //console.log(meta);
+              star.aka = meta.also_known_as;
+              star.biography = meta.biography;
+              star.birthday = meta.birthday;
+              star.birthplace = meta.place_of_birth;
+               //console.log(star);
+          callback(err, star);
         });
-        //console.log(actor);
-        actor.save(function(err, actor) {
-            if (err) return console.error(err);
+
+      },
+      /* let's get all the movies where the star played */
+      function (star, callback) {
+        request(apiUrl+"person/"+star._id+"/movie_credits"+"?api_key="+apiKey, function (err, response, body) {
+          if(err) return next(error);
+          var playedIn = JSON.parse(body);
+          star.movies = playedIn.cast;
+          callback(err, star);
         });
+      }
+    ], function (err, star) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      star.save(function(err, star) {
+        if (err) return;
       });
       done();
     });
-    
+  
 });
 
-agenda.schedule('daily pull', 'get latest', 'person/popular').repeatEvery('1 days');
-agenda.start();
+var dailyPull = agenda.schedule('daily pull', 'get latest', 'person/popular');
+    dailyPull.repeatEvery('0 16 15 * *').save();
+
+//agenda.start();
 
 agenda.on('start', function(job) {
   console.log("Job %s starting", job.attrs.name);
-  console.log(JSON.stringify(job));
+  //console.log(JSON.stringify(job));
 });
 
 agenda.on('complete', function(job) {
@@ -114,48 +190,70 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'www')));
 
 
-var actors = [
-    {
-        _id: 1,
-        name: "Samuel L. Jackson",
-        movies: [
-            { title: "Turbo", year: "2013" },
-            { title: "Avengers", year: "2012" }
-        ]
-    },
-    {
-        _id: 2,
-        name: "Bill Murray",
-        movies: [
-            { title: "Ghostbusters", year: "1985" },
-            { title: "Me, Myself and I", year: "2002" }
-        ]
-    }
-];
+// var stars = [
+//     {
+//         _id: 1,
+//         name: "Samuel L. Jackson",
+//         movies: [
+//             { title: "Turbo", year: "2013" },
+//             { title: "Avengers", year: "2012" }
+//         ]
+//     },
+//     {
+//         _id: 2,
+//         name: "Bill Murray",
+//         movies: [
+//             { title: "Ghostbusters", year: "1985" },
+//             { title: "Me, Myself and I", year: "2002" }
+//         ]
+//     }
+// ];
 
 // api routes
 
-// all actors
+// all stars
 app.get('/api/actors', function(req, res, next) {
-  var query = Actor.find();
+  var query = Star.find();
   query.limit(30);
-  query.exec(function(err, actors) {
+  query.exec(function(err, stars) {
     if (err) return next(err);
-    res.send(actors);
+    res.send(stars);
   });
 });
 
-// get one actor
-app.get('/api/actors/:id', function(req, res, next) {
-    Actor.findById(req.params.id, function(err, actor) {
+// get one star
+app.get('/api/actors/:_id/:slug', function(req, res, next) {
+    console.log(req.params);
+    Star.findById(req.params._id, function(err, star) {
         if(err)
             return next(err);
-        //console.log(actor);
-        res.send(actor);
+        console.log(star);
+        res.send(star);
     });
+});
+
+// get a movie
+app.get('/api/movies/:_id/:slug', function (req, res, next) {
+  Movie.findById(req.params._id, function (err, movie) {
+    if (err) return next(err);
+    if(!!movie) {
+      res.send(movie);
+    } else {
+      request(apiUrl+"movie/"+req.params._id+"?api_key="+apiKey, function (err, response, body) {
+        if(err) return next(err);
+        var _movie = JSON.parse(body);
+        console.log(_movie);
+        var movie = new Movie(_movie);
+        movie.save(function(err, m) {
+          if (err) return;
+        });
+        res.send(movie);
+      });
+    }
+  });
 });
 
 app.get('/hello', function(req, res, next) {
